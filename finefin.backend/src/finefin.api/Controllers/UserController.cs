e6c07d2ca899f0c04cfa5110e003e56254f12be7
João Vidal <dev.joaovidal@gmail.com>
2025-03-14 23:49:46 -0300

    valet package setup

--- finefin.backend/src/finefin.api/Controllers/UserController.cs
+++ finefin.backend/src/finefin.api/Controllers/UserController.cs
@@ -1,7 +1,6 @@
 ﻿using finefin.api.Http.Requests;
 using finefin.api.Http.Responses;
 using finefin.api.Providers.Services.UserServices.Register;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace finefin.api.Controllers
